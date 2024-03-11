cfa2ce8172724c7c05efab2ff73c5dfe8b7caa46
samir <sc9018@ic.ac.uk>
2024-03-11 17:20:43 +0000

    added dataloader of MNIST

--- src/basics/gen_mnist_sde/mixer_classification_demo.py
+++ src/basics/gen_mnist_sde/mixer_classification_demo.py
@@ -1,9 +1,25 @@
 import torch.nn as nn 
 import torch 
-import torchvision 
+from torchvision import datasets
+from torch.utils.data import DataLoader
 import numpy as np
 import matplotlib.pyplot as plt
 from einops import rearrange
 from mixer import MLPMixer
 
+#import MNIST
+train_set = datasets.MNIST(root='./data', train=True, download=True)
+train_loader = DataLoader(train_set,batch_size=16, shuffle=True)
 
+test_set = datasets.MNIST(root='./data', train=False, download=True)
+test_loader = DataLoader(train_set,batch_size=16, shuffle=False)
+
+
+
+#load into dataloader 
+
+#Write training loop
+
+# Call training loop
+
+#test model inference
