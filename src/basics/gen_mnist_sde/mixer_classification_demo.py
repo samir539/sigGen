f71e36595297fc7c776afbe455bc7e2c7d19f317
samir <sc9018@ic.ac.uk>
2024-03-12 11:49:36 +0000

    added mixer classifier for MNIST with working training loop

--- src/basics/gen_mnist_sde/mixer_classification_demo.py
+++ src/basics/gen_mnist_sde/mixer_classification_demo.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 from einops import rearrange
 from mixer import MLPMixer
 import torch.optim as optim
+from tqdm import tqdm
 
 #import MNIST and load into dataloader
 transform = transforms.Compose([
@@ -21,18 +22,19 @@ test_loader = DataLoader(train_set,batch_size=16, shuffle=False)
 #model params
 data_iter = iter(train_loader)
 images,label = data_iter.next()
+print("hellohello")
 first_img = images[0]
 first_label = label[0]
 image_height, image_width = first_img.shape[-2], first_img.shape[-1]
 channels = first_img.shape[0]
 channel_dim = 21 #hidden val
-patch_dim = 8
+patch_dim = 4
 patch_mlp_dim = 32
 channel_mlp_dim = 32
 mixer_layer_num = 2
 class_num = 10
 
-learning_rate = 0.01
+learning_rate = 0.1
 
 
 #init model
@@ -48,9 +50,30 @@ optimiser = optim.Adam(MLP_mixer_model.parameters(),lr=learning_rate)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 MLP_mixer_model.to(device)
 
-epoch_num = 5
+epoch_num = 20
+print("hello")
 
- 
+for i in tqdm(range(epoch_num)):
+    MLP_mixer_model.train() #put into train mode
+    running_loss = 0.0
+    print("hello2")
+    for images,labels in train_loader:
+        images,labels = images.to(device), labels.to(device)    #put data on device 
+
+        #forward pass
+        output = MLP_mixer_model(images)
+        loss = loss_criterion(output,labels)
+
+        #backward pass
+        optimiser.zero_grad()
+        loss.backward()
+        optimiser.step()
+
+        running_loss += loss.item()
+        # print(loss.item())
+        # print("hello",i)
+    epoch_loss = running_loss/len(train_loader)
+    print(f"The loss for epoch {i} is {epoch_loss:.4} ")
 
 #Write training loop
 
