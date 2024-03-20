3d80f6e120cf6fafce499e806bc65f9b404be08d
samir <sc9018@ic.ac.uk>
2024-03-20 16:35:39 +0000

    update perturbation kernel

--- src/basics/gen_mnist_sde/mixer_diffusion_model.py
+++ src/basics/gen_mnist_sde/mixer_diffusion_model.py
@@ -15,24 +15,31 @@ def perturbation_kernel(data,int_beta,t):
     """
     Compute the mean and standard deviation of the perturbation kernel given a t val
     :param data: a data sample
-    :param int_beta: int_beta
+    :param int_beta: function int_beta
     :param t: the time t to compute the sample value from p(x(t)|x(0))
     :return mean: the mean of the perturbation kernel
     :return std: return the standard deviation of the perturbation kernel 
     """
-    mean = data*np.exp(-0.5*int_beta)
-    std = 1 - np.exp(-int_beta)
+    mean = data*np.exp(-0.5*int_beta(t))
+    std = 1 - np.exp(-int_beta(t))
     return mean, std
 
 
 
 
 #loss function
-def loss_function(data,int_beta,):
+def loss_function(data,int_beta,weight,score_model):
     """
-    Function to compute the loss function
-    :param 
+    Function to compute the loss 
+    :param data: a batch of the data dim:[n_sample, height, width]
     """
+    t = torch.rand(data.shape[0])
+    mean,std = perturbation_kernel(data,int_beta,t)
+    #sample t
+    #perturbed x val
+    #score
+    #
+    
 
 
 #batch loss function
